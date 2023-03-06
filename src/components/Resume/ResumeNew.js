33a80aaa6988eabee49d9fee547978b14ab5b02e
Gaurav Padaliya <gauravpadaliya2000@gmail.com>
2023-03-06 14:56:53 +0530

    resume issue solvindg

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,18 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "../../Assets/../Assets/Resume03_23.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
+const resumeLink1 =
+  "https://housingfactory.s3.ap-south-1.amazonaws.com/attendanceFile/2022/1a2fd8ad-d13e-457b-9f7d-b0fada3a32b0.pdf";
+// const resumeLink =
+//   "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+  "https://raw.githubusercontent.com/gaurav-padaliya/Portfolio-gaurav/main/src/Assets/Resume03_23.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
